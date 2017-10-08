ea0c3e70cc3244a27d7795b6b5d17eb60f1006a7
Chris Wong <lambda.fairy@gmail.com>
2017-10-08 22:37:00 +1300

    🧝

---
+++ elf.js
@@ -0,0 +1,26 @@
+const TAU = 2 * Math.PI
+const CYCLE_LENGTH_MS = 19 * 1000
+const NOISE_FACTOR = 0.2
+
+let start
+
+function strobe(now) {
+  if (!start) start = now
+  const time = now - start
+  const noise = NOISE_FACTOR * Math.random()
+  const light = noise + Math.cos(time / CYCLE_LENGTH_MS * TAU)
+  document.documentElement.classList[light < 0 ? 'add' : 'remove']('dark')
+  requestAnimationFrame(strobe)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.documentElement.style.background = 'white'
+  document.getElementById('warning').style.display = 'block'
+  document.getElementById('everything').style.display = 'none'
+  document.getElementById('view').addEventListener('click', () => {
+    document.documentElement.style.background = ''
+    document.getElementById('warning').style.display = ''
+    document.getElementById('everything').style.display = ''
+    requestAnimationFrame(strobe)
+  })
+})
